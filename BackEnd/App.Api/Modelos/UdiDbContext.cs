2244db86293f49aef9eea37655ba14a0f0b2237b
wkrea <william.trigos@hotmail.com>
2020-11-20 17:41:48 -0500

    Actualización: * Agregando proyecto App.Api

---
+++ BackEnd/App.Api/Modelos/UdiDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace App.Api.Modelos
+{
+    public class UdiDbContext : DbContext
+    {
+        public UdiDbContext(DbContextOptions<UdiDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Escuela> Escuelas { get; set; }
+        public DbSet<Profesor> Profesores { get; set; }
+        public DbSet<Curso> Cursos { get; set; }
+        public DbSet<Estudiante> Estudiantes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+        }
+    }
+}
