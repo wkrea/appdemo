f9d91675f5b482a3028046f22dcd6caaf00b57b7
wkrea <william.trigos@hotmail.com>
2020-11-20 17:41:48 -0500

    Actualización: * Agregando proyecto App.Api

--- BackEnd/App.Api/Program.cs
+++ BackEnd/App.Api/Program.cs
@@ -1,11 +1,10 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace App.Api
 {
