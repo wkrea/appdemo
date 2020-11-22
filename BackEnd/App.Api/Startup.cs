71be5db715eb6e53f6fc438d4e724682d7a1e6ba
Personal <afcelis7210@gmail.com>
2020-11-22 12:47:20 -0500

    Modificación en Startup

--- BackEnd/App.Api/Startup.cs
+++ BackEnd/App.Api/Startup.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using App.Api.Modelos;
+using Microsoft.EntityFrameworkCore; 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -10,13 +8,22 @@ using Microsoft.Extensions.Hosting;
 
 namespace App.Api
 {
-    public class Startup
+  public class Startup
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddJsonOptions(option => 
+            {
+                option.JsonSerializerOptions.IgnoreNullValues = true;
+                option.JsonSerializerOptions.WriteIndented    = true;
+            });
+
+            services.AddDbContext<UdiDbContext>(opts => 
+                opts.UseInMemoryDatabase("UdiDb-Memory")
+                , ServiceLifetime.Singleton
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
