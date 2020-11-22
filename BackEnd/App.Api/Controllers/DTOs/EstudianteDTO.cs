22c13a8897b413a38a35d1f32fcbc3f6d9ee84f7
Wilmer Arevalo <wilmerarevalorincon@gmail.com>
2020-11-22 17:28:10 -0500

    Cambios en EstudianteDTO

--- BackEnd/App.Api/Controllers/DTOs/EstudianteDTO.cs
+++ BackEnd/App.Api/Controllers/DTOs/EstudianteDTO.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace App.Api.Controllers.DTOs
+﻿namespace App.Api.Controllers.DTOs
 {
     public class EstudianteDTO
     {
