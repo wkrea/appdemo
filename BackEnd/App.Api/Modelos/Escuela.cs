b98739d4b2a10e9a73f0d813eee04f0776f0bfdd
jplata24 <jplata2@udi.edu.co>
2020-11-24 11:18:26 -0500

    PrimeraEntregaParcial

---
+++ BackEnd/App.Api/Modelos/Escuela.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+namespace App.Api.Modelos
+{
+    public class Escuela
+    {
+        public int Id {get; set;}
+        public string Nombre { get; set;}
+        public string Ciudad {get; set;}
+        public string Departamento {get; set;}
+        public ICollection <Profesor> Profesores {get; set;}
+
+    }
+}
