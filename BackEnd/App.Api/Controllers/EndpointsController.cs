b96e1551bc742b6821262b8a9d4a02e1f8239ec0
Personal <afcelis7210@gmail.com>
2020-11-24 21:12:35 -0500

    Modificación de Controlador

--- BackEnd/App.Api/Controllers/EndpointsController.cs
+++ BackEnd/App.Api/Controllers/EndpointsController.cs
@@ -2,27 +2,25 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
-name space App.Api.Controllers
+namespace App.Api.Controllers
 {
     [ApiController]
-    [Router("")] //http://localhost:5000
-
-    public class EndpointController : ControllerBase
+    [Route("")] //http://localhost:5000
+    public class EndpointsController : ControllerBase
     {
         private readonly ILogger<EndpointsController> archivoLog;
-        PUblic EndpointsController(ILogger<EndpointsController> logInicializar) 
+        public EndpointsController(ILogger<EndpointsController> logInicializar) 
         {
             archivoLog = logInicializar;
         }
 
         [HttpGet]
-
         public object Get(){
             var objetoRespuesta = new {
                 Status = "Api está en linea"
-            }
+            };
             archivoLog.LogInformation(objetoRespuesta.Status);
-            return new {};        
+            return objetoRespuesta;        
         }
     }
 }
