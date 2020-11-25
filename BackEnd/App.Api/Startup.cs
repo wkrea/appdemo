3be20409b4d09fc8e3d877c75c58e9fcb370059e
MarlyAcosta <61189229+MarlyAcosta@users.noreply.github.com>
2020-11-25 18:06:33 -0500

    Cambios para correcion de errores

--- BackEnd/App.Api/Startup.cs
+++ BackEnd/App.Api/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using App.Api.Modelos;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,6 +18,10 @@ namespace App.Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+            services.AddDbContext<UdiDbContext>(opts =>
+             opts.UseInMemoryDatabase("UdiDbContext")
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
