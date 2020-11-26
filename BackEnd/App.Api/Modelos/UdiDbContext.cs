a105a18c692c55f7a15167a3a7c3d2ee202518cf
gabocastro29 <55069295+gabocastro29@users.noreply.github.com>
2020-11-26 16:21:02 -0500

    Modelo UdiDbContext
    se modifico la clase UdiDbContext y se crearon los Dbset de los demas modelos

--- BackEnd/App.Api/Modelos/UdiDbContext.cs
+++ BackEnd/App.Api/Modelos/UdiDbContext.cs
@@ -3,6 +3,32 @@ namespace App.Api.Modelos
 {
     public class UdiDbContext : DbContext
     {
-        
+        public UdiDbContext(DbContextOptions<UdiDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Curso> Cursos {get; set;}
+        public DbSet<Escuela> Escuelas {get; set;}
+        public DbSet<Estudiante> Estudiantes {get; set;}
+        public DbSet<Profesor> Profesores {get; set;}
+
+        protected override void OnModelCreating(ModelBuilder builder){
+            builder.Entity<Curso>().HasKey(p => p.Id);
+            builder.Entity<Curso>().HasData(
+                new Curso(){Id = 1, Nombre = "Servicios Web"},
+                new Curso(){Id = 2, Nombre = "Sistemas Operativos"}
+            );
+            builder.Entity<Escuela>().HasData(
+                new Escuela(){Id = 1, Nombre = "Universidad de Investigacion y Desarrollo", Ciudad = "Bucaramanga", Departamento = "Santander"}
+            );
+            builder.Entity<Estudiante>().HasData(
+                new Estudiante(){Id = 1, Nombre = "Gabriel Alexander Castro Vargas"}
+            );
+            builder.Entity<Profesor>().HasData(
+                new Profesor(){Id = 1, Nombre = "William Javier Trigos Guevara"}
+            );
+        }
+
     }
 }
