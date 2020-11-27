0dc792f7ffbde2407f70ad35a38c76d774d695a2
MarlyAcosta <61189229+MarlyAcosta@users.noreply.github.com>
2020-11-27 17:03:03 -0500

    Creacion de la logica del repositorio del estudiante

--- BackEnd/App.Api/Repositorios/EstudianteRepo.cs
+++ BackEnd/App.Api/Repositorios/EstudianteRepo.cs
@@ -1,7 +1,46 @@
+using App.Api.Modelos;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
 namespace App.Api.Repositorios
 {
     public class EstudianteRepo : IEstudianteRepo
-    {
+    {  
+        private readonly UdiDbContext _db;
+        
+        public EstudianteRepo(UdiDbContext context)
+        {
+            _db = context;
+        } 
 
+        public async Task<Estudiante> obtenerEstudiante(int id)
+        {
+            return await _db.estudiantes.FindAsync(id);
+        }
+
+        public async Task<List<Estudiante>> obtenerEstudiantes()
+        {
+            return await _db.estudiantes.ToListAsync();
+        }
+
+        public async Task crearEstudiante(Estudiante estudiante)
+        {
+            _db.estudiantes.Add(estudiante);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task editarEstudiante(Estudiante estudiante)
+        {
+            _db.estudiantes.Update(estudiante);
+            await _db.SaveChangesAsync();
+        }
+
+        public async Task eliminarEstudiante(int id)
+        {
+            Estudiante estudiante =_db.estudiantes.Find(id);
+            _db.estudiantes.Remove(estudiante);
+            await _db.SaveChangesAsync();
+        }
     }
 }
--- BackEnd/App.Api/Repositorios/IEstudianteRepo.cs
+++ BackEnd/App.Api/Repositorios/IEstudianteRepo.cs
@@ -1,5 +1,24 @@
-namespace App.Api.Repositorios{
-    public interface IEstudianteRepo{
-        
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using App.Api.Modelos;
+
+namespace App.Api.Repositorios
+{
+    public interface IEstudianteRepo
+    {
+        //Get estudiante especifico
+        Task<Estudiante> obtenerEstudiante(int id);
+
+        //Get lista de estudiantes      
+        Task<List<Estudiante>> obtenerEstudiantes();
+
+        //Post crear estudiante
+        Task crearEstudiante(Estudiante estudiante);
+
+        //Put estudiante
+        Task editarEstudiante(Estudiante estudiante);
+
+        //Delete estudiante
+        Task eliminarEstudiante(int id);
     }
 }
