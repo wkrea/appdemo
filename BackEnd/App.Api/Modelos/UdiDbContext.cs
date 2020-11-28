e48ae75b6346475f9d8b1ca1f58690362ce4f9e7
MarlyAcosta <61189229+MarlyAcosta@users.noreply.github.com>
2020-11-28 15:43:59 -0500

    Arreglo del dbcontext con los parametros establecidos por el profesor

--- BackEnd/App.Api/Modelos/UdiDbContext.cs
+++ BackEnd/App.Api/Modelos/UdiDbContext.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.Tracing;
+using System.IO;
 using System.Collections.Immutable;
 using Microsoft.EntityFrameworkCore;
 
@@ -10,32 +12,73 @@ namespace App.Api.Modelos
             Database.EnsureCreated();
         }
 
-        public DbSet<Curso> Cursos {get; set;}
-        public DbSet<Escuela> Escuelas {get; set;}
-        public DbSet<Profesor> Profesores {get; set;}
-        public DbSet<Estudiante>  Estudiantes {get; set;}
+        public DbSet<Curso> cursos {get; set;}
+        public DbSet<Escuela> escuelas {get; set;}
+        public DbSet<Profesor> profesores {get; set;}
+        public DbSet<Estudiante>  estudiantes {get; set;}
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            builder.Entity<Curso>().HasKey(p => p.Id);
+            /*
+                -Cuales son los campos requeridos en los modelos
+                -Nombre
+                -Ciudad
+                 No permitir que el id no se autogenere
+            */
+            //Id no autogenerado
+            builder.Entity<Escuela>(esc =>
+            {
+                esc.HasKey(e => e.Id);
+                esc.Property(p => p.Id).ValueGeneratedNever();
+            });
+            builder.Entity<Estudiante>(est =>
+            {
+                est.HasKey(e => e.Id);
+                est.Property(p => p.Id).ValueGeneratedNever();
+            });
+            builder.Entity<Curso>(est =>
+            {
+                est.HasKey(c => c.Id);
+                est.Property(p => p.Id).ValueGeneratedNever();
+            });
+            builder.Entity<Profesor>(est =>
+            {
+                est.HasKey(p => p.Id);
+                est.Property(p => p.Id).ValueGeneratedNever();
+            });
+
+            //Relacion entre tablas
+            builder.Entity<Curso>().HasMany(c => c.Estudiantes).WithOne(est => est.Curso);
+            builder.Entity<Estudiante>().HasOne(e => e.Curso).WithMany(cur => cur.Estudiantes);
+
+            builder.Entity<Curso>().HasOne(c => c.Profesor).WithMany(pro => pro.Cursos);
+            builder.Entity<Profesor>().HasMany(p => p.Cursos).WithOne(cur => cur.Profesor);
+
+            builder.Entity<Escuela>().HasMany(e => e.Profesores).WithOne(pro => pro.Escuela);
+            builder.Entity<Profesor>().HasOne(p => p.Escuela).WithMany(esc => esc.Profesores);
+
+            //Campos requeridos
+            builder.Entity<Escuela>().Property(esc => esc.Nombre).IsRequired();
+            builder.Entity<Escuela>().Property(esc => esc.Ciudad).IsRequired();
+            builder.Entity<Estudiante>().Property(est => est.Nombre).IsRequired();
+            builder.Entity<Profesor>().Property(pro => pro.Nombre).IsRequired();
+            builder.Entity<Curso>().Property(cur => cur.Nombre).IsRequired();
+
+            //Creacion de los datos que siempre estaran en la base de datos
             builder.Entity<Curso>().HasData(
-                new Curso(){ Id = 1, Nombre = "Sistemas Operativos"},
-                new Curso(){ Id = 2, Nombre = "Servicios Web"},
-                new Curso(){ Id = 3, Nombre = "Inteligencia Artificial I"}
+                new Curso(){ Id = 1, Nombre = "6L"},
+                new Curso(){ Id = 2, Nombre = "7L"}
             );
             builder.Entity<Escuela>().HasData(
-                new Escuela(){ Id = 1, Nombre = "Universidad de Investigación y Desarrollo", Ciudad = "Bucaramanga", Departamento = "Santander"},
-                new Escuela(){ Id = 2, Nombre = "Universidad Industrial de Santander", Ciudad = "Bucaramanga", Departamento = "Santander"}
+                new Escuela(){ Id = 1, Nombre = "Universidad de Investigación y Desarrollo", Ciudad = "Bucaramanga", Departamento = "Santander"}
             );
-            /* builder.Entity<Profesor>().HasData(
-                
+            builder.Entity<Profesor>().HasData(
+                new Profesor(){ Id=1, Nombre="William Javier Trigos Guevara", EscuelaId = 1 },
+                new Profesor(){ Id=2, Nombre="Alexandra Beltran", EscuelaId = 1 }
             );
             builder.Entity<Estudiante>().HasData(
-
-            ) */;
-            
+                new Estudiante(){ Id = 1, Nombre = "Marly Alexandra Acosta Arenales", CursoId = 1}
+            );
         }
-
-
     }
 }
