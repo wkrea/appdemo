d35171a6a08c45d7337bfaf9bbe3d8ae0febc241
DubanFlp <dubanfelipem@gmail.com>
2020-11-28 17:29:05 -0500

    Creacion de Dbset

--- BackEnd/App.Api/Modelos/UdiDbContext.cs
+++ BackEnd/App.Api/Modelos/UdiDbContext.cs
@@ -3,6 +3,16 @@ using Microsoft.EntityFrameworkCore;
 namespace App.Api.Modelos
 {
     public class UdiDbContext : DbContext{
+        public DbSet<Curso> cursos {get;set;}
+        public DbSet<Estudiante> estudiantes {get;set;}
+
+        public DbSet<Profesor> profesores {get;set;}
+        public DbSet<Escuela> escuelas {get;set;}
+
+        protected override void OnModelCreating(ModelBuilder builder){
         
+        
+        );
+        }
     }
 }
