f566199b02ef4b4a82314b9a27cdc574fa6d0a5d
MarlyAcosta <61189229+MarlyAcosta@users.noreply.github.com>
2020-11-27 20:07:50 -0500

    logica del controlador usando el patron repositorio con cada uno de los metodos del crud

--- BackEnd/App.Api/Controllers/EstudiantesController.cs
+++ BackEnd/App.Api/Controllers/EstudiantesController.cs
@@ -1,10 +1,83 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using App.Api.Modelos;
+using App.Api.Repositorios;
 using Microsoft.AspNetCore.Mvc;
 
 namespace App.Api.Controllers
 {
-    public class EstudiantesController : ControllerBase{
-        
+    [ApiController]
+    [Route("[controller]")] //http://localhost:5000/Personas
+    public class EstudiantesController : ControllerBase
+    {
+        private readonly IEstudianteRepo context;
+        public EstudiantesController(IEstudianteRepo estudianteRepo)
+        {
+            context = estudianteRepo;
+        }
+        [HttpGet]
+        public async Task<ActionResult<List<Estudiante>>> Get()
+        {
+            return await context.obtenerEstudiantes();
+        }
 
-        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Estudiante>> GetById(int id)
+        {
+            return await context.obtenerEstudiante(id);
+        }
+        [HttpPost]
+        public async Task<ActionResult> Crear([FromBody] Estudiante estudiante)
+        {
+            try
+            {
+                await context.crearEstudiante(estudiante);
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Editar(int id, [FromBody] Estudiante estEditado)
+        {
+            try
+            {
+                var existe = context.obtenerEstudiante(id);
+                if(existe == null){
+                    return NotFound();
+                }
+                if(existe.Id != id){
+                    return BadRequest();
+                }
+                await context.editarEstudiante(estEditado);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Eliminar(int id)
+        {
+            try
+            {
+                var existe = context.obtenerEstudiante(id);
+                if(existe == null){
+                    return NotFound();
+                }
+                if(existe.Id != id){
+                    return BadRequest();
+                }
+                await context.eliminarEstudiante(id);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+            return Ok();
+        }
     }
 }
