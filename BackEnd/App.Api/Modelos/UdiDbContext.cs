6fc9fd27c1b81be4886e70fc7d8f9ab3c269614c
DubanFlp <dubanfelipem@gmail.com>
2020-11-28 18:30:38 -0500

    Stratup

--- BackEnd/App.Api/Modelos/UdiDbContext.cs
+++ BackEnd/App.Api/Modelos/UdiDbContext.cs
@@ -3,9 +3,13 @@ using Microsoft.EntityFrameworkCore;
 namespace App.Api.Modelos
 {
     public class UdiDbContext : DbContext{
+
+        public UdiDbContext(DbContextOptions<UdiDbContext> opts) : base(opts)
+        {
+            Database.EnsureCreated(); 
+        }
         public DbSet<Curso> cursos {get;set;}
         public DbSet<Estudiante> estudiantes {get;set;}
-
         public DbSet<Profesor> profesores {get;set;}
         public DbSet<Escuela> escuelas {get;set;}
 
