f2ead2a436c712774666af6170caa79b8ff553d6
mazterdx <75100344+mazterdx@users.noreply.github.com>
2020-11-29 18:50:35 -0500

    Agregando Clases Base

--- BackEnd/App.Api/Modelos/curso.cs
+++ BackEnd/App.Api/Modelos/curso.cs
@@ -1,15 +1,19 @@
-﻿
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 
 namespace App.Api.Modelos
 {
-    public class curso
+    public class Curso
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
-        public int ProfesorId { get; set; }
-        public Profesor Profesor { get; set; }
-        public ICollection<Estudiante> Estudiantes { get; set; }
 
+        /// <summary>
+        /// Establecer relación completa (no requiere FluentApi)
+        /// https://henriquesd.medium.com/entity-framework-core-relationships-with-fluent-api-8f741c57b881
+        /// </summary>
+        public int ProfesorId { get; set; }
+        public virtual Profesor Profesor { get; set; }
+
+        public virtual ICollection<Estudiante> Estudiantes { get; set; }
     }
 }
