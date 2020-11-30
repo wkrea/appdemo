96af9d2f446cb7b61d82fee78e242c738b23e280
mazterdx <75100344+mazterdx@users.noreply.github.com>
2020-11-29 21:55:29 -0500

    2da clase

---
+++ BackEnd/App.Api/Controllers/EndpointsController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace App.Api.Controllers
+{  
+    [ApiController]
+    [Route("")]
+    public class EndpointsController : ControllerBase
+    {
+        private readonly ILogger<EndpointsController> archivoLog;
+
+        public EndpointsController(ILogger<EndpointsController> LogInicializar)
+        {
+            archivoLog = LogInicializar;
+        }
+        [HttpGet]
+
+        public object Get()
+        {
+            var objetoRespuesta = new {
+                Status = "Hola gente"
+        };
+            archivoLog.LogInformation(objetoRespuesta.Status);
+            return objetoRespuesta;
+        }
+    }
+    
+    
+}
