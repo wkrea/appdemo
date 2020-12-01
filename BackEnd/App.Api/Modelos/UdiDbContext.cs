0513a7e27de9064da381ca0171554aef0c052113
ValenciaJCamilo <camilovalncias@gmail.com>
2020-12-01 06:26:24 -0500

    Cambio UdiDbContext

--- BackEnd/App.Api/Modelos/UdiDbContext.cs
+++ BackEnd/App.Api/Modelos/UdiDbContext.cs
@@ -15,7 +15,15 @@ namespace App.Api.Modelos
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
-            builder.Entity<Curso>().HasKey(p => p.Id);
+
+            //CURSO
+            builder.Entity<Curso>().Property(c => c.Nombre).IsRequired();
+            builder.Entity<Curso>()
+                   .HasMany(c => c.Estudiantes)
+                   .WithOne(est => est.Curso);
+            builder.Entity<Curso>()
+                   .HasOne(c => c.Profesor)
+                   .WithMany(p => p.Cursos);
             builder.Entity<Curso>().HasData(
                 new Curso(){ Id = 1, Nombre = "Servicios Web"},
                 new Curso(){ Id = 2, Nombre = "Estadisticas y probabilidades"},
@@ -25,6 +33,13 @@ namespace App.Api.Modelos
                 new Curso(){ Id = 6, Nombre = "Metodología de la investigación"}
             );
 
+            //ESCUELA
+            builder.Entity<Escuela>().Property(e => e.Nombre).IsRequired();
+            builder.Entity<Escuela>().Property(e => e.Ciudad).IsRequired();
+            builder.Entity<Escuela>().Property(e => e.Departamento).IsRequired();
+            builder.Entity<Escuela>()
+                    .HasMany(e => e.Profesores)
+                    .WithOne(p => p.Escuela);                    
             builder.Entity<Escuela>().HasData(
                 new Escuela(){ Id = 1, Nombre = "Universidad Pontificia Bolivariana", Ciudad = "Bucaramanga", Departamento = "Santander"},
                 new Escuela(){ Id = 2, Nombre = "Universidad de Santander", Ciudad = "Bucaramanga", Departamento = "Santander"},
@@ -33,6 +48,11 @@ namespace App.Api.Modelos
                 new Escuela(){ Id = 5, Nombre = "Universidad Nacional", Ciudad = "Bogotá", Departamento = "Cundinamarca"}         
             );
 
+            //ESTUDIANTE
+            builder.Entity<Estudiante>().Property(est => est.Nombre).IsRequired();
+            builder.Entity<Estudiante>()
+                   .HasOne(est => est.Curso)
+                   .WithMany(c => c.Estudiantes);
             builder.Entity<Estudiante>().HasData(
                 new Estudiante(){ Id = 1, Nombre = "Juan Camilo Valencia Silva"},
                 new Estudiante(){ Id = 2, Nombre = "Laura Juliana Lozano Calderón"},
@@ -44,6 +64,14 @@ namespace App.Api.Modelos
                 new Estudiante(){ Id = 8, Nombre = "Pedro Pablo Perez Pereira"}
             );
 
+            //PROFESOR
+            builder.Entity<Profesor>().Property(p => p.Nombre).IsRequired();
+            builder.Entity<Profesor>()
+                   .HasMany(p => p.Cursos)
+                   .WithOne(c => c.Profesor);
+            builder.Entity<Profesor>()
+                   .HasOne(p => p.Escuela)
+                   .WithMany(e => e.Profesores);
             builder.Entity<Profesor>().HasData(
                 new Profesor(){ Id = 1, Nombre = "William Javier Trigos Guevara"},
                 new Profesor(){ Id = 2, Nombre = "Martín Perez Jaimes"},
