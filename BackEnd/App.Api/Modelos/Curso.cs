8b3ed9987f28e7e23503b47f90c8d1db467c3429
mazterdx <75100344+mazterdx@users.noreply.github.com>
2020-12-01 20:25:51 -0500

    last commit 1/12/2020

--- BackEnd/App.Api/Modelos/Curso.cs
+++ BackEnd/App.Api/Modelos/Curso.cs
@@ -1,18 +1,15 @@
-﻿using System.Collections.Generic;
+﻿
+using System.Collections.Generic;
 
 namespace App.Api.Modelos
 {
-    public class Curso
+    public class curso
     {
         public int Id { get; set; }
         public string Nombre { get; set; }
-
-        /// <summary>
-        /// Establecer relación completa (no requiere FluentApi)
-        /// </summary>
         public int ProfesorId { get; set; }
         public Profesor Profesor { get; set; }
-
         public ICollection<Estudiante> Estudiantes { get; set; }
+
     }
 }
