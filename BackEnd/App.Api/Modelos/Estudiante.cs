7ec592c0513552761bf919d8a1baa8591db7ab60
DanielSuarez <danielandressuarezquintana@gmail.com>
2020-12-01 22:30:24 -0500

    Update

---
+++ BackEnd/App.Api/Modelos/Estudiante.cs
@@ -0,0 +1,14 @@
+﻿namespace App.Api.Modelos
+{
+    public class Estudiante
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+
+        /// <summary>
+        /// Establecer relación completa (no requiere FluentApi)
+        /// </summary>
+        public int CursoId { get; set; }
+        public virtual Curso Curso { get; set; }
+    }
+}
